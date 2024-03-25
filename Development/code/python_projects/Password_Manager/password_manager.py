0e7c96dfba7eed3c9888e2db738344641dd93067
JonahPeck <PeckJonahC@gmail.com>
2024-03-25 10:47:09 -0400

    add function start

---
+++ Development/code/python_projects/Password_Manager/password_manager.py
@@ -0,0 +1,23 @@
+master_pwd = input("What is the master password? ")
+
+def view():
+    pass 
+
+def add():
+    name = input('Account Name: ')
+    pwd = input('Password: ')
+
+    with open('passwords.txt', 'a') as f:
+        
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, or add), press 'q' to quit ? ")
+    if mode == 'q':
+        break
+
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print("Invalid mode")
