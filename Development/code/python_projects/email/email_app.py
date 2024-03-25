a7bc1e6e6882fd98c1f72194f281bf343584092d
JonahPeck <PeckJonahC@gmail.com>
2024-03-24 22:43:28 -0400

    cool

--- Development/code/python_projects/email/email_app.py
+++ Development/code/python_projects/email/email_app.py
@@ -24,3 +24,4 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
 
+#this was cool
